c008618caf689eedb30bd07ed272eca43a819868
Marek Rzepnicki <mrzepnicki@p29-10.labpk.inf.ug.edu.pl>
2015-10-03 12:30:04 +0200

    tail -n 8 program.c

---
+++ c/program.c
@@ -0,0 +1,21 @@
+{
+
+char operator;
+
+float value1, value2;
+
+float result;
+
+printf("Witaj w kalkutorze 4-funkcyjnym\n\n");
+
+printf("Podaj pierwsza liczbe, operator dzialania i druga liczbe: \n");
+
+fgets(line, sizeof(line), stdin);
+
+sscanf(line, "%f %c %f",&value1, &operator, &value2);
+
+switch(operator)
+
+{int a,b,c
+
+  cout<a;
